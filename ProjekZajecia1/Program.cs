3d384846876a04225baec14e172b0c917d66f529
Artur Żywczak <artzywcz@gmail.com>
2021-02-27 12:54:08 +0100

    Add project files.

---
+++ ProjekZajecia1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ProjekZajecia1Server {
+    class Program {
+        static void Main(string[] args) {
+            
+            //--dane servera
+            const int PORT_NO = 8001;
+            const string SERVER_IP = "192.168.195.167";
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+
+            
+            Console.WriteLine("Czekam na polaczenie");
+            listener.Start();
+            Socket s = listener.AcceptSocket();
+
+            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+
+        }
+    }
+}
---
+++ ProjektZajecia1Client/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjektZajecia1Client {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            TcpClient client = new TcpClient();
+            var result = client.BeginConnect("192.168.195.167", 8001, null, null); //Spróbuj połączyć z serverem
+            var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(5)); //True jesli się połączy, czeka 5s
+            success = client.Connected;
+            if (!success) { //jeśli nie połączy
+                //client.EndConnect(result);
+                label1.Text = "Nie dziala Sadge";
+
+            }
+            else label1.Text = "Dziala WidePeepoHappy";
+            //client.EndConnect(result);
+        }
+    }
+}
